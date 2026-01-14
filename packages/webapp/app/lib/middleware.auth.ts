014f6abc70a6398561e7db34f4e2ceab42303c34
Drew DeCarme <drewdecarme@gmail.com>
2026-01-13 23:38:14 -0500

    updates

--- packages/webapp/app/lib/middleware.auth.ts
+++ packages/webapp/app/lib/middleware.auth.ts
@@ -6,26 +6,22 @@ import type { ContextAndRequest } from "./types";
 export async function requireSession<T extends ContextAndRequest>(args: T) {
   const cf = await getCFContext(args);
 
-  console.log(args.request.headers);
-
   const url = `${cf.env.API_DOMAIN}/api/auth/get-session`;
   const res = await fetch(url, {
     headers: args.request.headers,
     credentials: "include",
   });
-  console.log(res);
 
   if (!res.ok) {
     throw redirect("/sign-in");
   }
 
   const session = (await res.json()) as SessionContext;
-  console.log(session);
   if (!session || !session.session) {
     throw redirect("/sign-in");
   }
 
-  // // Set the session data in context
-  // // The session data structure from better-auth API
+  // Set the session data in context
+  // The session data structure from better-auth API
   args.context.set(sessionContext, session);
 }
